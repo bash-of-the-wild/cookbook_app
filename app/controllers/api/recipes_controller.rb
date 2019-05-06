ceef06aec1f45d59ead44c986e6a471d952fdde0
JackMarx <seriousfools@gmail.com>
2019-05-06 09:37:52 -0500

    index and show

---
+++ app/controllers/api/recipes_controller.rb
@@ -0,0 +1,11 @@
+class Api::RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+end
